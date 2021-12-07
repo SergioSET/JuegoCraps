03aac56794d0bf6a1226f7012956c741c442d15e
20204081 Sergio Escudero Tabares <sergio.escudero@correounivalle.edu.co>
2021-12-06 19:25:27 -0500

    added classes GUI and Escucha

--- src/juegoCraps/GUI.java
+++ src/juegoCraps/GUI.java
@@ -2,26 +2,44 @@ package juegoCraps;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 /**
- * This class is used for ...
- * @autor Paola-J Rodriguez-C paola.rodriguez@correounivalle.edu.co
- * @version v.1.0.0 date:21/11/2021
+ * This class is used as view Craps Class
+ *
+ * @version v.1.0.0 date:06/12/2021
+ * @autor Sergio Escudero Tabares sergio.escudero@correounivalle.edu.co
  */
 public class GUI extends JFrame {
+    private static final String MENSAJE_INICIO = "Bienvenido a Craps"
+            + "\n Oprime el botón lanzar para iniciar el juego"
+            + "\n Si tu tiro de salida es de 7 u 11 ganas con Natural"
+            + "\n Si tu tiro de salida es 2, 3 u 12 pierdes con Craps"
+            + "\n Si sacas cualquier otro valor establecerás el Punto"
+            + "\n Estado en punto podrás seguir lanzando los dados"
+            + "\n pero ahora ganarás si sacas nuevamente el valor del Punto"
+            + "\n sin que previamente hayas sacado 7";
 
     private Header headerProject;
+    private JLabel dado1, dado2;
+    private JButton lanzar;
+    private JPanel panelDados, panelResultados;
+    private ImageIcon imageDado;
+    private JTextArea resultados;
+    private Escucha escucha;
+    private ModelCraps modelCraps;
 
     /**
      * Constructor of GUI class
      */
-    public GUI(){
+    public GUI() {
         initGUI();
 
         //Default JFrame configuration
-        this.setTitle("The Title app");
-        this.setSize(200,100);
-        //this.pack();
+        this.setTitle("Juego Craps");
+        //this.setSize(200,100);
+        this.pack();
         this.setResizable(true);
         this.setVisible(true);
         this.setLocationRelativeTo(null);
@@ -34,19 +52,44 @@ public class GUI extends JFrame {
      */
     private void initGUI() {
         //Set up JFrame Container's Layout
-        //Create Listener Object and Control Object
+        //Create Listener Object or Control Object
+        escucha = new Escucha();
+        modelCraps = new ModelCraps();
         //Set up JComponents
-        headerProject = new Header("Header ...", Color.BLACK);
+        headerProject = new Header("Mesa Juego Craps", Color.BLACK);
+        this.add(headerProject, BorderLayout.NORTH);
 
-        this.add(headerProject,BorderLayout.NORTH); //Change this line if you change JFrame Container's Layout
+        imageDado = new ImageIcon(getClass().getResource("/resources/dado.png"));
+        dado1 = new JLabel(imageDado);
+        dado2 = new JLabel(imageDado);
+
+        lanzar = new JButton("lanzar");
+        lanzar.addActionListener(escucha);
+
+        panelDados = new JPanel();
+        panelDados.setPreferredSize(new Dimension(300, 180));
+        panelDados.setBorder(BorderFactory.createTitledBorder("Tus Dados"));
+        panelDados.add(dado1);
+        panelDados.add(dado2);
+        panelDados.add(lanzar);
+
+        this.add(panelDados, BorderLayout.CENTER);
+
+        resultados = new JTextArea(7, 31);
+        resultados.setText(MENSAJE_INICIO);
+        resultados.setBorder(BorderFactory.createTitledBorder("Que debes hacer"));
+        JScrollPane scroll = new JScrollPane(resultados);
+        this.add(scroll, BorderLayout.EAST);
+
     }
 
     /**
      * Main process of the Java program
+     *
      * @param args Object used in order to send input data from command line when
      *             the program is execute by console.
      */
-    public static void main(String[] args){
+    public static void main(String[] args) {
         EventQueue.invokeLater(() -> {
             GUI miProjectGUI = new GUI();
         });
@@ -55,7 +98,20 @@ public class GUI extends JFrame {
     /**
      * inner class that extends an Adapter Class or implements Listeners used by GUI class
      */
-    private class Escucha {
+    private class Escucha implements ActionListener {
 
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            modelCraps.calcularTiro();
+            int[] caras = modelCraps.getCaras();
+            imageDado = new ImageIcon(getClass().getResource("/resources/"+caras[0]+".png"));
+            dado1.setIcon(imageDado);
+            imageDado = new ImageIcon(getClass().getResource("/resources/"+caras[1]+".png"));
+            dado2.setIcon(imageDado);
+
+            modelCraps.determinarJuego();
+            resultados.setText(modelCraps.getEstadoToString());
+
+        }
     }
 }
